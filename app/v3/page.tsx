14122c8c522f507866ca39344d7875095ea9f560
alexandre <alex.fauquette@gmail.com>
2023-05-03 21:53:45 +0200

    show request body

--- app/v3/page.tsx
+++ app/v3/page.tsx
@@ -14,9 +14,10 @@ import { RootState } from "@/redux/store";
 import ingredientsSlice from "@/redux/reducers/ingredients";
 import IngredientCard from "@/components/IngredientCardV3";
 import { Button, Stack } from "@mui/material";
-import { openEditor } from "@/redux/reducers/editor";
+import { DataType, openEditor } from "@/redux/reducers/editor";
 import { Sheet } from "@mui/joy";
 import { parseURLParameters } from "@/redux/reducers/recipes_v2";
+import data from "../../data/ingredient_taxonomy.json";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -58,6 +59,47 @@ export default function Home() {
           <IngredientCard {...ingredient} key={ingredient.id} />
         ))}
       </Stack>
+      <h3 style={{ padding: 20 }}>Request sent:</h3>
+      <pre style={{ paddingLeft: 50 }}>
+        {"To: https://world.openfoodfacts.dev/api/v3/product/test\n\n"}
+        body:{" "}
+        {JSON.stringify(
+          {
+            lc: "en",
+            fields: "ingredients,nutriments_estimated,nutriscore_grade",
+            product: {
+              categories_tags: ["fruits"],
+              ingredients_text_en: ingrdients
+                .flatMap((ingredient) => {
+                  const ingredientType = (data as DataType).find(
+                    (type) => type["Ingredient type id"] === ingredient.typeId
+                  )!;
+                  const ingredientData = ingredientType.ingredients.find(
+                    (ing) => ing["Ingredient id"] === ingredient.id
+                  )!;
+
+                  return ingredient.quantities.map((quantity) => {
+                    const quantityData = ingredientData.quantities.find(
+                      (q) => q["Quantity id"] === quantity.id
+                    )!;
+                    const isPerUnit = !!quantityData.default_weight_per_unit;
+
+                    const weight =
+                      quantity.value *
+                      (isPerUnit
+                        ? parseInt(quantityData.default_weight_per_unit)
+                        : 1);
+
+                    return `${ingredientData.Ingredient} ${weight}g`;
+                  });
+                })
+                .join(", "),
+            },
+          },
+          null,
+          2
+        )}
+      </pre>
       <Sheet
         sx={{
           p: 2,
