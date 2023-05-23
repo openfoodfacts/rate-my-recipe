c8539fca807a93977dff4f7b3d2fa97813b110f9
alexandre <alex.fauquette@gmail.com>
2023-05-23 22:19:13 +0200

    add support for `null` nutriscore (when no request is sent)

--- components/Nutriscore.tsx
+++ components/Nutriscore.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Nutriscore({ grade = "empty", height = 80 }) {
   return (
     <Image
-      src={`/nutriscores/nutriscore-${grade}.svg`}
+      src={`/nutriscores/nutriscore-${grade || "empty"}.svg`}
       alt="nutriscore"
       width={(240 / 130) * height}
       height={height}
