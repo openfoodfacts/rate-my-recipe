8d483298d1e6f84b8088ae8f092a1528fe0de00f
alexandre <alex.fauquette@gmail.com>
2023-05-23 22:15:51 +0200

    Update table to show evolution

--- components/ShowNutritionalTable.tsx
+++ components/ShowNutritionalTable.tsx
@@ -5,6 +5,9 @@ import Nutriscore from "@/components/Nutriscore";
 import { Button, Drawer, Stack, drawerClasses } from "@mui/material";
 import Table from "@mui/joy/Table";
 import Sheet from "@mui/joy/Sheet";
+import DecreaseIcon from "@mui/icons-material/SouthEast";
+import IncreaseIcon from "@mui/icons-material/NorthEast";
+import StableIcon from "@mui/icons-material/East";
 
 const NUTRIMENTS_TITLE: { [key: string]: string } = {
   "energy-kcal_100g": "Energie (kCal)",
@@ -29,6 +32,42 @@ const NUTRIMENTS_PRECISION = {
   proteins_100g: 1,
   salt_100g: 1,
 } as any;
+
+const shouldIncrease = {
+  fiber_100g: true,
+  proteins_100g: true,
+} as any;
+
+const EvolutionIcon = ({
+  userValue,
+  urlValue,
+  nutrimentKey,
+}: {
+  userValue?: number;
+  urlValue?: number;
+  nutrimentKey: string;
+}) => {
+  if (urlValue === undefined || userValue === undefined) {
+    return null;
+  }
+  if (urlValue < userValue) {
+    return (
+      <IncreaseIcon
+        color={shouldIncrease[nutrimentKey] ? "success" : "error"}
+      />
+    );
+  }
+
+  if (urlValue < userValue) {
+    return (
+      <DecreaseIcon
+        color={shouldIncrease[nutrimentKey] ? "error" : "success"}
+      />
+    );
+  }
+
+  return <StableIcon color="inherit" />;
+};
 
 export default function ShowNutritionalTable() {
   const userNutriscore = useSelector(
@@ -66,34 +105,60 @@ export default function ShowNutritionalTable() {
           },
         }}
       >
-        <Table aria-label="basic table">
+        <Table
+          aria-label="basic table"
+          sx={{ "& span": { whiteSpace: "initial" } }}
+        >
           <thead>
             <tr>
               <th>Nutriment</th>
-              <th>Valeur initiale (pour 100 g)</th>
-              <th>Valeur modifiée (pour 100 g)</th>
+              <th>
+                <span>Valeur initiale (pour 100 g)</span>
+              </th>
+              <th>
+                <span>Valeur modifiée (pour 100 g)</span>
+              </th>
             </tr>
           </thead>
           <tbody>
-            {Object.keys(NUTRIMENTS_TITLE).map((key) => (
-              <tr key={key}>
-                <td
-                  style={{
-                    paddingLeft: NUTRIMENTS_TITLE[key].includes("dont")
-                      ? 10
-                      : 0,
-                  }}
-                >
-                  {NUTRIMENTS_TITLE[key]}
-                </td>
-                <td>
-                  {urlNutriments?.[key]?.toFixed(NUTRIMENTS_PRECISION[key])}
-                </td>
-                <td>
-                  {userNutriments?.[key]?.toFixed(NUTRIMENTS_PRECISION[key])}
-                </td>
-              </tr>
-            ))}
+            {Object.keys(NUTRIMENTS_TITLE).map((key) => {
+              const urlValue = urlNutriments?.[key]?.toFixed(
+                NUTRIMENTS_PRECISION[key]
+              );
+              const userValue = userNutriments?.[key]?.toFixed(
+                NUTRIMENTS_PRECISION[key]
+              );
+              return (
+                <tr key={key}>
+                  <td
+                    style={{
+                      paddingLeft: NUTRIMENTS_TITLE[key].includes("dont")
+                        ? 10
+                        : 0,
+                    }}
+                  >
+                    {NUTRIMENTS_TITLE[key]}
+                  </td>
+                  <td>
+                    <span>{urlValue}</span>
+                  </td>
+                  <td
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <span>{userValue}</span>
+                    <EvolutionIcon
+                      urlValue={urlValue}
+                      userValue={userValue}
+                      nutrimentKey={key}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </Table>
         <Sheet
