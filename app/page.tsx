6b34d5b45a5ab0f37adb98f7a1ee0402848c8d0d
alexandre <alex.fauquette@gmail.com>
2023-05-31 20:19:47 +0200

    main page layout updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,19 @@ import IngredientSelector from "@/components/IngredientSelector";
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectCurrentIngredients, selectURLParams } from "@/redux/selectors";
-import store, { RootState } from "@/redux/store";
+import { RootState } from "@/redux/store";
 import IngredientCard from "@/components/IngredientCard";
-import { Button, Icon, Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { openEditor } from "@/redux/reducers/editor";
 import { Sheet } from "@mui/joy";
 import { updateRecipeIngredients } from "@/redux/reducers/recipes";
 import ShowNutritionalTable from "@/components/ShowNutritionalTable";
-import Add from '@mui/icons-material/Add';
-import ShareIcon from '@mui/icons-material/Share';
-import Typography from "@mui/joy/Typography";
-import Grid from "@mui/joy/Grid";
+import Add from "@mui/icons-material/Add";
+import AppBar from "@/components/AppBar";
 
 export default function Home() {
   const dispatch = useDispatch();
-  const currentRecipeId = "userRecipe";
   const searchParams = useSearchParams();
-  const params = useSelector((state: RootState) =>
-    selectURLParams(state, currentRecipeId)
-  );
   React.useEffect(() => {
     dispatch<any>(
       updateRecipeIngredients({
@@ -55,69 +49,19 @@ export default function Home() {
   const ingrdients = useSelector((state: RootState) =>
     selectCurrentIngredients(state, "userRecipe")
   );
-  function handleShareButtonClick() {
-    const url = "https://amathjourney.com/api/yololo";
-
-    const ingredients = selectCurrentIngredients(
-      store.getState(),
-      currentRecipeId
-    );
-
-    const valuesAndQuantities = ingredients.flatMap((ingredient) =>
-      ingredient.quantities.map((quantity) => ({
-        quantity: quantity.id,
-        value: quantity.value,
-      }))
-    );
-    const body = JSON.stringify({ valuesAndQuantities });
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: body,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        return data;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
 
   return (
     <main
       style={{
         maxWidth: "100%",
       }}
-     >
-     <Grid container justifyContent="center" margin="20px">
-    <Grid container spacing={2} padding="10px">
-      <Grid>
-        <Button variant="contained" startIcon={<ShareIcon />} onClick={handleShareButtonClick}>
-          Share Recipe
-        </Button>
-      </Grid>
-      <Grid>
-        <Typography>
-          url:{" "}
-          <a href={`https://splendorous-beijinho-051f54.netlify.app?${params}`} style={{ color: "blue" }}>
-            {`https://splendorous-beijinho-051f54.netlify.app?${params}`} 
-            <Icon sx={{ color: "red" }} />
-          </a>
-        </Typography>
-      </Grid>
-    </Grid>
-  </Grid>
-      <Stack direction="row" flexWrap="wrap" >
+    >
+      <AppBar />
+      <Stack direction="row" flexWrap="wrap">
         {ingrdients.map((ingredient) => (
           <IngredientCard {...ingredient} key={ingredient.id} />
         ))}
       </Stack>
-      <h3 style={{ padding: 20 }}>Request sent:</h3>
-
       <Sheet
         sx={{
           p: 2,
@@ -125,12 +69,16 @@ export default function Home() {
           bottom: 0,
           width: "100%",
           textAlign: "center",
-          marginTop: "20px" ,
-          
+          marginTop: "20px",
         }}
-       >
-      
-        <Button variant="contained" startIcon={<Add />} onClick={() => dispatch(openEditor({}))} >Add Ingredient</Button>
+      >
+        <Button
+          variant="contained"
+          startIcon={<Add />}
+          onClick={() => dispatch(openEditor({}))}
+        >
+          Add Ingredient
+        </Button>
         <ShowNutritionalTable />
       </Sheet>
       <IngredientSelector />
--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,23 +1,112 @@
 "use client";
 
+import * as React from "react";
+import { useSearchParams } from "next/navigation";
+import IngredientSelector from "@/components/IngredientSelector";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentIngredients, selectURLParams } from "@/redux/selectors";
+import store, { RootState } from "@/redux/store";
+import IngredientCard from "@/components/IngredientCard";
+import { Button, Icon, Snackbar, Stack } from "@mui/material";
+import { openEditor } from "@/redux/reducers/editor";
+import { updateRecipeIngredients } from "@/redux/reducers/recipes";
+import ShowNutritionalTable from "@/components/ShowNutritionalTable";
+
 import Box from "@mui/joy/Box";
 
-const AppBar = () => (
-  <Box
-    sx={{
-      width: "100%",
-      height: "54px",
-      backgroundColor: "#76B947", // Set the desired background color
-      color: "#fff", // Set the desired text color
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: "20px",
-      fontWeight: "bold",
-    }}
-  >
-    Rate My Recipes
-  </Box>
-);
+import ShareIcon from "@mui/icons-material/Share";
+import SaveIcon from "@mui/icons-material/Save";
+import Typography from "@mui/joy/Typography";
+import Grid from "@mui/joy/Grid";
+import Sheet from "@mui/joy/Sheet";
+import { IconButton, Tooltip } from "@mui/joy";
+
+const AppBar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const params = useSelector((state: RootState) =>
+    selectURLParams(state, "userRecipe")
+  );
+  function handleShareButtonClick() {
+    const url = "https://amathjourney.com/api/yololo";
+
+    const ingredients = selectCurrentIngredients(
+      store.getState(),
+      "userRecipe"
+    );
+
+    const valuesAndQuantities = ingredients.flatMap((ingredient) =>
+      ingredient.quantities.map((quantity) => ({
+        quantity: quantity.id,
+        value: quantity.value,
+      }))
+    );
+    const body = JSON.stringify({ valuesAndQuantities });
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  return (
+    <Sheet
+      variant="solid"
+      color="primary"
+      invertedColors
+      sx={(theme) => ({
+        width: "100%",
+        height: "54px",
+        ...theme.typography.h5,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: 2,
+      })}
+    >
+      Rate My Recipes
+      <Box>
+        <Tooltip title="Partager un lien">
+          <IconButton
+            sx={{ mr: 1 }}
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${window.location.origin}${window.location.pathname}?${params}`
+              );
+              setOpen(true);
+            }}
+          >
+            <ShareIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Enregistrer la recette">
+          <IconButton
+            onClick={() => {
+              handleShareButtonClick();
+            }}
+          >
+            <SaveIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={() => setOpen(false)}
+        message="url copié dans le press papier"
+      />
+    </Sheet>
+  );
+};
 
 export default AppBar;
