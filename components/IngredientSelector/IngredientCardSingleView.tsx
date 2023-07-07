bdfd2f88a10276ba1a72f96c534968b330ee12dd
Shmigolk <75388692+Shmigolk@users.noreply.github.com>
2023-07-07 16:29:22 +0300

    refactoring

--- components/IngredientSelector/IngredientCardSingleView.tsx
+++ components/IngredientSelector/IngredientCardSingleView.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import { Input, Stack, Typography } from "@mui/joy";
+import { CardContent, Input, Stack, Typography } from "@mui/joy";
 import { ValueEditor } from "../shared/molecules/ValueEditor";
 import { ChangeEvent } from "react";
 import { ImagePlaceholder } from "@/components/shared/atoms/ImagePlaceholder";
+import Card from "@mui/joy/Card";
+import { CardMedia } from "@mui/material";
 
 interface QuantityCardProps {
   onIncrement(): void;
@@ -25,42 +27,47 @@ export function IngredientCardSingleView({
   step = 2,
 }: QuantityCardProps) {
   return (
-    <Stack
-      alignItems="center"
-      width={"fit-content"}
-      alignSelf={"center"}
-      maxWidth={"100%"}
-      overflow={"scroll"}
+    <Card
       sx={{
         overflowY: "scroll",
         "::-webkit-scrollbar": { display: "none" },
+        alignItems: "center",
+        width: "fit-content",
+        alignSelf: "center",
+        overflow: "scroll",
+        border: "1px solid lightGrey",
       }}
     >
-      <Typography component={"h1"} fontSize={"x-large"}>
-        {title}
-      </Typography>
+      <CardContent>
+        <Typography component={"h1"} fontSize={"x-large"}>
+          {title}
+        </Typography>
+      </CardContent>
       {imgSrc ? (
-        <img
-          src={imgSrc}
-          alt="A beautiful landscape."
-          style={{ maxHeight: "70%" }}
+        <CardMedia
+          component="img"
+          height={"70%"}
+          image={imgSrc || ""}
+          alt={title}
         />
       ) : (
         <ImagePlaceholder placeholderText={"No image sorry"} />
       )}
 
-      <ValueEditor
-        onIncrement={onIncrement}
-        onDecrement={onDecrement}
-        disabledDecrement={quantityValue! <= step}
-      >
-        <Input
-          type="number"
-          value={quantityValue!}
-          onChange={onInputChange}
-          endDecorator={<Typography>{unit}</Typography>}
-        />
-      </ValueEditor>
-    </Stack>
+      <CardContent>
+        <ValueEditor
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          disabledDecrement={quantityValue! <= step}
+        >
+          <Input
+            type="number"
+            value={quantityValue!}
+            onChange={onInputChange}
+            endDecorator={<Typography>{unit}</Typography>}
+          />
+        </ValueEditor>
+      </CardContent>
+    </Card>
   );
 }
