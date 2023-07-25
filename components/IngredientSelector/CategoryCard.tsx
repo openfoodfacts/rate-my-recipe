bb11373a795d8f11ae2ae900a80cbfe2b2b9381d
Konstantin <75388692+Shmigolk@users.noreply.github.com>
2023-07-25 12:31:29 +0300

    Feature/create iconss for categories cards (#43)

--- components/IngredientSelector/CategoryCard.tsx
+++ components/IngredientSelector/CategoryCard.tsx
@@ -1,18 +1,55 @@
 import { GenericCard } from "@/components/shared/molecules/GenericCard";
+import { BeefIcon } from "@/components/shared/atoms/icons/BeefIcon";
+import { FishIcon } from "@/components/shared/atoms/icons/FishIcon";
+import { EggIcon } from "@/components/shared/atoms/icons/EggIcon";
+import { BroccoliIcon } from "@/components/shared/atoms/icons/BroccoliIcon";
+import { CarrotIcon } from "@/components/shared/atoms/icons/CarrotIcon";
+import { EggplantIcon } from "@/components/shared/atoms/icons/EggplantIcon";
+import { FlourIcon } from "@/components/shared/atoms/icons/FlourIcon";
+import { CerealsIcon } from "@/components/shared/atoms/icons/CerealsIcon";
+import { OliveOil } from "@/components/shared/atoms/icons/OliveOil";
+import { Butter } from "@/components/shared/atoms/icons/Butter";
+import { WineBottle } from "@/components/shared/atoms/icons/WineBottle";
+import { Milk } from "@/components/shared/atoms/icons/Milk";
+import { SaltAndPaper } from "@/components/shared/atoms/icons/SaltAndPaper";
+import { SaladIcon } from "@/components/shared/atoms/icons/SaladIcon";
+import { LeafIcon } from "@/components/shared/atoms/icons/LeafIcon";
 
 interface CategoryCardProps {
   title: string | undefined;
   onClick(): void;
+  categoryId: string;
 }
 
-export function CategoryCard({ title, onClick }: CategoryCardProps) {
+export function CategoryCard({
+  title,
+  onClick,
+  categoryId,
+}: CategoryCardProps) {
+  const IconsMap: {
+    [key: string]: JSX.Element[];
+  } = {
+    "meat-fish-eggs": [
+      <BeefIcon key={"beef"} />,
+      <EggIcon key={"egg"} />,
+      <FishIcon key={"fish"} />,
+    ],
+    vegetables: [
+      <BroccoliIcon key={"broccoli"} />,
+      <CarrotIcon key={"carrot"} />,
+      <EggplantIcon key={"eggplant"} />,
+    ],
+    "potatoes-legumes-cereals": [
+      <FlourIcon key={"flour"} />,
+      <CerealsIcon key={"cereals"} />,
+    ],
+    fats: [<OliveOil key={"oliveoil"} />, <Butter key={"butter"} />],
+    liquids: [<WineBottle key={"wine"} />, <Milk key={"milk"} />],
+    "spices-condiments": [<SaltAndPaper key={"salt"} />],
+    "fresh-herbs": [<LeafIcon key={"leaf"} />, <SaladIcon key={"salad"} />],
+  };
+
   return (
-    <GenericCard
-      title={title}
-      onClick={onClick}
-      imgUrl={
-        "https://thomasgabin.com/wp-content/uploads/2023/04/Open-food-Logo.png"
-      }
-    />
+    <GenericCard title={title} onClick={onClick} svg={IconsMap[categoryId]} />
   );
 }
---
+++ components/shared/atoms/icons/Milk.tsx
@@ -0,0 +1,38 @@
+export const Milk = () => (
+  <svg
+    fill="#000000"
+    width="50px"
+    height="50px"
+    viewBox="0 0 24 24"
+    id="milk-box-left"
+    data-name="Flat Line"
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon flat-line"
+  >
+    <path
+      id="secondary"
+      d="M6,15v5a1,1,0,0,0,1,1H17a1,1,0,0,0,1-1V11C12,11,12,15,6,15Z"
+      fill="rgb(44, 169, 188)"
+      strokeWidth={2}
+    ></path>
+    <path
+      id="primary"
+      d="M6,15c6,0,6-4,12-4"
+      fill="none"
+      stroke="rgb(0, 0, 0)"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    ></path>
+    <path
+      id="primary-2"
+      data-name="primary"
+      d="M11,5H8V3h3ZM7,21H17a1,1,0,0,0,1-1V6a1,1,0,0,0-1-1H7A1,1,0,0,0,6,6V20A1,1,0,0,0,7,21Z"
+      fill="none"
+      stroke="rgb(0, 0, 0)"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    ></path>
+  </svg>
+);
