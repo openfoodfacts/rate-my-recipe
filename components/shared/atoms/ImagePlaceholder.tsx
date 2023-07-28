9eeec4705cf16f2697f661a4c896a52230ed3744
Shmigolk <75388692+Shmigolk@users.noreply.github.com>
2023-07-28 16:28:50 +0300

    feature: added placeholder

--- components/shared/atoms/ImagePlaceholder.tsx
+++ components/shared/atoms/ImagePlaceholder.tsx
@@ -3,15 +3,17 @@ import * as React from "react";
 
 export const ImagePlaceholder = ({
   placeholderText,
+  height,
 }: {
   placeholderText?: string;
+  height?: number;
 }) => {
   return (
     <Stack
       sx={(theme) => ({
         width: "100%",
         bgcolor: theme.colorSchemes.light.palette.primary[100],
-        height: 150,
+        height: height || 150,
       })}
     >
       {placeholderText}
--- components/shared/molecules/GenericCard.tsx
+++ components/shared/molecules/GenericCard.tsx
@@ -2,6 +2,7 @@ import Card from "@mui/joy/Card";
 import { CardMedia } from "@mui/material";
 import { CardContent, Stack, Typography } from "@mui/joy";
 import React from "react";
+import { ImagePlaceholder } from "@/components/shared/atoms/ImagePlaceholder";
 
 interface GenericCardProps {
   title: string | undefined;
@@ -31,6 +32,7 @@ export function GenericCard({ title, onClick, imgUrl, svg }: GenericCardProps) {
           alt={title}
         />
       )}
+      {!imgUrl && !svg && <ImagePlaceholder height={70} />}
       <CardContent>
         {svg && (
           <Stack direction={"row"} justifyContent={"center"} gap={1}>
